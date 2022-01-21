3ba7c48258d964886f669ddd60e95f39ac45c885
Денис Григорьев <Denisgri25@outlook.com>
2022-01-21 17:59:42 +0300

    Add project files.

---
+++ For-Space-Lovers/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using For_Space_Lovers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace For_Space_Lovers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ For-Space-Lovers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace For_Space_Lovers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
