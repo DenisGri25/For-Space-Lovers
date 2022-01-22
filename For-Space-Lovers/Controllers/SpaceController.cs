3115d8b02d50fffe9196e928ffd2557fc186c08c
Денис Григорьев <Denisgri25@outlook.com>
2022-01-22 03:27:45 +0300

    add page with Astronomy Picture of the Day

---
+++ For-Space-Lovers/Controllers/SpaceController.cs
@@ -0,0 +1,20 @@
+﻿using For_Space_Lovers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace For_Space_Lovers.Controllers
+{
+    public class SpaceController : Controller
+    {
+        private SpaceDataLayer _spaceDataLayer;
+
+        public SpaceController()
+        {
+            _spaceDataLayer = new SpaceDataLayer();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var svm = await _spaceDataLayer.GetAdopUrlAsync();
+            return View(svm);
+        }
+    }
+}
---
+++ For-Space-Lovers/Models/SpaceVievModel.cs
@@ -0,0 +1,8 @@
+﻿namespace For_Space_Lovers.Models;
+
+public class SpaceVievModel
+{
+    public string? Url { get; set; }
+    public string? Explanation { get; set; }
+    public string? Title { get; set; }
+}
